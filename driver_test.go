e61c92f37a439fba5a4d821b60478df41f20cbef
Wes Freeman <wes@skeweredrook.com>
2013-12-08 03:25:55 -0500

    adding more tests. array results don't work.

--- driver_test.go
+++ driver_test.go
@@ -15,31 +15,80 @@ func testConn() *sql.DB {
 	return db
 }
 
-func TestQuerySimple(t *testing.T) {
+func prepareTest(query string) *sql.Stmt {
 	db := testConn()
-	stmt, err := db.Prepare("return 1")
+	stmt, err := db.Prepare(query)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
+	return stmt
+}
 
+func prepareAndQuery(query string) *sql.Rows {
+	stmt := prepareTest(query)
 	rows, err := stmt.Query()
 	if err != nil {
+		log.Fatal(err)
+	}
+	return rows
+}
+
+func failIfErr(err error, t *testing.T) {
+	if err != nil {
 		t.Fatal(err)
 	}
+}
 
+func TestQuerySimple(t *testing.T) {
+	rows := prepareAndQuery("return 1")
 	hasNext := rows.Next()
 	if !hasNext {
 		t.Fatal("no next!")
 	}
 
 	var test int
-	err = rows.Scan(&test)
-	if err != nil {
-		t.Fatal(err)
-	}
+	err := rows.Scan(&test)
+	failIfErr(err, t)
 
 	if test != 1 {
 		t.Fatal("test != 1")
 	}
-
 }
+
+func TestQuerySimpleFloat(t *testing.T) {
+	rows := prepareAndQuery("return 1.2")
+	rows.Next()
+	var test float64
+	err := rows.Scan(&test)
+	failIfErr(err, t)
+
+	if test != 1.2 {
+		t.Fatal("test != 1.2")
+	}
+}
+
+func TestQuerySimpleString(t *testing.T) {
+	rows := prepareAndQuery("return '123'")
+	rows.Next()
+	var test string
+	err := rows.Scan(&test)
+	failIfErr(err, t)
+
+	if test != "123" {
+		t.Fatal("test != '123'")
+	}
+}
+
+// TODO array conversion
+/*
+func TestQuerySimpleIntArray(t *testing.T) {
+	rows := prepareAndQuery("return [1,2,3]")
+	rows.Next()
+	var test []int
+	err := rows.Scan(&test)
+	failIfErr(err, t)
+
+	if test[0] != 1 || test[1] != 2 || test[2] != 3 {
+		t.Fatal("test != [1,2,3];", test)
+	}
+} */
